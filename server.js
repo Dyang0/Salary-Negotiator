55a97df63072c975d48fe8aff8ce8c312b795a30
samanthah05 <samanthah05@vt.edu>
2025-02-01 16:54:02 -0500

    bye bye not so good login

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const cors = require("cors");
-
+// const mongoose = require("mongoose");
+// const jwt = require("jsonwebtoken");
+// const cors = require("cors");
+//
 const app = express();
-app.use(express.json());
-app.use(cors());
+//app.use(express.json());
+// app.use(cors());
 
-// MongoDB connection
-mongoose.connect("mongodb://localhost:27017/salary_negotiation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+//MongoDB connection
+// mongoose.connect("mongodb://localhost:27017/salary_negotiation", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+//
+app.use(express.static("public"));
 // Start server
 const PORT = 3000;
 app.listen(PORT, () => {
